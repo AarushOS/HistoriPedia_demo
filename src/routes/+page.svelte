238d515e3025cbfa50ddc7c5b93683e1b73d1971
AarushOS <79082249+AarushOS@users.noreply.github.com>
2023-01-08 23:38:00 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,4 +30,18 @@ h1{
     0% {background-position: 0%}
     100% {background-position: 100%}
 }
+
+@media only screen and (max-width: 720px) {
+    h1{
+        font-size: 4rem;
+        position: absolute;
+        transform: translate(-50%, 50%);
+        left: 50%;
+        top: 17.5%;
+        color: #f4f3ef;
+        user-select: none;
+        margin-bottom: 10px;
+        z-index: 9;
+    }
+}
 </style>
